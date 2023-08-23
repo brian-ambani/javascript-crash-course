920ebd305080da8b239bff66925e1f24e7acda47
brian-ambani <brainambani1@gmail.com>
2023-08-23 16:04:52 +0300

    updated datatypes

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello js");
+// console.log("Hello js");
+
+// let score;
+// score = 32;
+
+// data types: string, numbers, Boolean, null, undefined
+
+const name = 'John';
+const age = 30;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
+let z; //undefined
+
+console.log(typeof x)
