5ace7f256658cccc9a1300754f90314fb5daf22c
brian-ambani <brainambani1@gmail.com>
2023-08-23 16:19:20 +0300

    updated js

--- main.js
+++ main.js
@@ -1,16 +1,42 @@
-// console.log("Hello js");
+console.log("Hello js");
 
-// let score;
-// score = 32;
+let score;
+score = 32;
 
 // data types: string, numbers, Boolean, null, undefined
 
-const name = 'John';
-const age = 30;
+// const name = 'John';
+// const age = 30;
 const rating = 4.5;
 const isCool = true;
 const x = null;
 const y = undefined;
 let z; //undefined
 
-console.log(typeof x)
+//console.log(typeof x)
+
+const name = 'John';
+const age = 30;
+
+// Template String
+const intro = (`my name is ${name} and I am ${age} years old`);
+
+//console.log(intro)
+
+
+const s = 'tech, computers, code, IT';
+
+// console.log(s.split(', '))
+
+
+// Arrays - variables that hold multiple values
+
+const fruits = ['Apples', 'oranges', 'pears'];
+
+// console.log(fruits[1])
+
+fruits.push('grapes');
+fruits.unshift('strawberries');
+fruits.pop();
+
+// console.log(fruits)
