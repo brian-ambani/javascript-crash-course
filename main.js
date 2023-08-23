fd50d8486ff424f93b7e7b6c9ff02d112e9d9997
brian-ambani <brainambani1@gmail.com>
2023-08-23 16:37:44 +0300

    updated js

--- main.js
+++ main.js
@@ -83,3 +83,16 @@ const todos = [
 const todoJSON = JSON.stringify(todos);
 
 // console.log(todoJSON);
+
+//For
+
+for(let i = 0; i < 10; i++) {
+    // console.log(`${i}`);
+}
+
+// while
+let i = 0;
+while(i < 10) {
+    // console.log(`${i}`);
+    i++;
+}
